db47975a4a5bffa36f9ba694423166b826a61ae2
gonzalosola@gmail.com <gonzalosola@gmail.com>
2019-11-16 22:53:38 -0300

    Estructura de un spring-boot con Swagger

---
+++ server/src/test/java/ar/gis/ApplicationTests.java
@@ -0,0 +1,12 @@
+package ar.gis;
+
+import ar.gis.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ server/src/test/java/ar/gis/common/AbstractIntegrationTest.java
@@ -0,0 +1,46 @@
+package ar.gis.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ContextConfiguration(initializers = {AbstractIntegrationTest.Initializer.class})
+@AutoConfigureMockMvc
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Container
+    public static MySQLContainer sqlContainer = new MySQLContainer("mysql:5.7.22")
+            .withDatabaseName("integration-tests-db")
+            .withUsername("sa")
+            .withPassword("sa");
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + sqlContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + sqlContainer.getUsername(),
+                    "spring.datasource.password=" + sqlContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+}
