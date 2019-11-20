ee0fcb57dad466f10d1d4e7a9a9b5f062649996e
Gonzalo Sola <gonzalosola@gmail.com>
2019-11-20 00:29:17 -0300

    Cambios de VueJS basico

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,9 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
 
-const Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
--- server/src/test/java/ar/gis/ApplicationTests.java
+++ server/src/test/java/ar/gis/ApplicationTests.java
@@ -1,7 +1,9 @@
 package ar.gis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ApplicationTests {
 
     @Test
