090b7529dc3c50753c2a529f34229d0601b6d7d6
Gonzalo Sola <gonzalosola@gmail.com>
2019-11-21 02:02:32 -0300

    Pagina vue inicial basica y agregando dependencias

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',    
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.component(Vuex)
+
 
 new Vue({
   render: h => h(App),
