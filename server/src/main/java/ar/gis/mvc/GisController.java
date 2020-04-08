7286d36f5d05774e9d94083cc9b9d1a441433bae
gonzalosola@gmail.com <gonzalosola@gmail.com>
2020-04-08 03:44:48 -0300

    Agregar el swagger en server. Agregando dependencias.

--- server/src/main/java/ar/gis/mvc/GisController.java
+++ server/src/main/java/ar/gis/mvc/GisController.java
@@ -1,16 +1,8 @@
 package ar.gis.mvc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.*;
+import org.slf4j.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GisController {
@@ -18,28 +10,19 @@ public class GisController {
     private Logger log = LoggerFactory.getLogger(GisController.class);
 
     @RequestMapping(value="/lista", method = RequestMethod.GET)
-    public @ResponseBody List<Object> getLista(@RequestParam(value="crimeCatId") Integer crimeCatId) throws Exception{    
-                try {
-                    //return this.crimeTypeManager.getCrimeTypeList(crimeCatId);
-                    return new ArrayList<Object>();
-                    //return "true";
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                    return null;
-                }
+    public @ResponseBody List<Object> getLista() throws Exception{
+        try {
+            return new ArrayList<>();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     @RequestMapping(value="/", method = RequestMethod.GET)
-    public @ResponseBody ModelAndView getRoot() throws Exception{    
-                try {
-                    //return this.crimeTypeManager.getCrimeTypeList(crimeCatId);
-                    //return new ArrayList<Object>();
-                    ModelAndView viewName = new ModelAndView();//this, null, Status.ACCEPTED);
-                    return viewName;
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                    return null;
-                }
+    //public @ResponseBody ModelAndView getRoot() throws Exception{ TODO: anterior template inicial, comentado para que funcione
+    public  String getRoot() {
+        return "/";
     }
 
 }
