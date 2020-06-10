29c5800a3cb2815e8a202198e83857a766d68e88
Gonzalo <gonzalosola@gmail.com>
2020-06-09 23:21:09 -0300

    Agregar Bootstrap-Vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
   const routes = [
@@ -9,15 +11,7 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
-  }/*,
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import( webpackChunkName: "about"  '../views/About.vue')
-  }*/
+  }
 ]
 
 const router = new VueRouter({
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,11 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>
+      <b-button second
+        src=""
+        name="algo">
+          Algo
+      </b-button>
+    </h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
