4d0cfe4f0393a6e06a3bb2f417f967d18fc9c700
Gonzalo <gonzalosola@gmail.com>
2020-06-10 02:01:34 -0300

    Agregar Axios y el css de bootstrap y bootstrap-vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(axios)
 
 Vue.config.productionTip = false
 
